6f8452702cceb5234a480b0f97e6c9efcbc722b2
sunita369 <sunita98mondal@gmail.com>
2021-06-08 02:08:40 +0530

    Searchination microservice added

---
+++ Searchination_Searchination/src/main/java/com/railway/Searchination/SearchinationApplication.java
@@ -0,0 +1,16 @@
+package com.railway.Searchination;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SearchinationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchinationApplication.class, args);
+	}
+
+}
---
+++ Searchination_Searchination/src/test/java/com/railway/Searchination/SearchinationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.railway.Searchination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchinationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
