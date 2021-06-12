24b71d73df4628e6e9636a20d40f981b3a8125e5
sunita369 <sunita98mondal@gmail.com>
2021-06-12 10:46:12 +0530

    swagger added

--- ApiGateway_ApiGateway/src/main/java/com/railway/ApiGateway/ApiGatewayApplication.java
+++ ApiGateway_ApiGateway/src/main/java/com/railway/ApiGateway/ApiGatewayApplication.java
@@ -1,13 +1,17 @@
 package com.railway.ApiGateway;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication(scanBasePackages = {"com.railway.ApiGateway.controller"})
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
 @EnableZuulProxy
 @EnableEurekaClient
+@EnableSwagger2
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
